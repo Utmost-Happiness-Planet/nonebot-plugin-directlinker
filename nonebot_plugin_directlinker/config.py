a5895c4c06be399b4d843b211b14aa07507e5807
SakuraPuare <java20131114@gmail.com>
2023-01-26 20:51:10 +0800

    🐞 fix: 拼写错误

--- nonebot_plugin_directlinker/config.py
+++ nonebot_plugin_directlinker/config.py
@@ -2,10 +2,10 @@ from nonebot import get_driver, logger
 
 config = get_driver().config.dict()
 
-if 'link_group' not in config:
-    logger.warning('[直链提取] 未发现配置项 `link_group` , 采用默认值: []')
-if 'link_command' not in config:
-    logger.warning('[直链提取] 未发现配置项 `link_command` , 采用默认值: "link"')
+if 'linker_group' not in config:
+    logger.warning('[直链提取] 未发现配置项 `linker_group` , 采用默认值: []')
+if 'linker_command' not in config:
+    logger.warning('[直链提取] 未发现配置项 `linker_command` , 采用默认值: "link"')
 
 
 linker_group = config.get('linker_group', [])
