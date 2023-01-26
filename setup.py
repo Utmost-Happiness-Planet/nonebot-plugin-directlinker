d8bf96c53720a52be40167ef03758d589b0d7bad
Kl1nge5 <2351859943@qq.com>
2023-01-26 20:16:31 +0800

    chore: update setup.py
    更新setup.py，迁移至uhp organization

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="nonebot-plugin-directlinker",
-    version="2.2.2",
-    author="Kl1nge5",
+    version="2.3.0",
+    author="uhpteam",
     description="A plugin based on NoneBot2 to extract direct links of files in qq group.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ninthseason/nonebot-plugin-directlinker",
+    url="https://github.com/Utmost-Happiness-Planet/nonebot-plugin-directlinker",
     project_urls={
-        "Bug Tracker": "https://github.com/ninthseason/nonebot-plugin-directlinker/issues",
+        "Bug Tracker": "https://github.com/Utmost-Happiness-Planet/nonebot-plugin-directlinker/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
